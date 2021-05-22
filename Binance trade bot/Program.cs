014f5da84544f16fa07953aa7f3bd307f961c067
DESKTOP-UH50LF4\mih0202 <mihail.damian@mail.com>
2021-05-23 00:13:28 +0300

    init project; init gitignore

---
+++ Binance trade bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Binance_trade_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
